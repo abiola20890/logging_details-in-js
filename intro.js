3db0d9927aa77bbb82a88cfbf87de9c446aeb5fe
Ibilola Abiola <ibilolaabiola75@gmail.com>
2025-11-05 23:04:30 +0100

    initial commit: Logging my Details

---
+++ intro.js
@@ -0,0 +1,4 @@
+let name = 'Abiola Ibilola'
+let age = 33
+let favouriteprogramminglanguage = 'JavaScript'
+console.log(`My name is ${name}, I am ${age} years old and my favourite programming language is ${favouriteprogramminglanguage}.`)
